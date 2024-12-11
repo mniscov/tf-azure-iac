e927cdb7e58fe5771043541800024a81f139e5af
mniscov <marian.niscov@kronsoft.ro>
2024-12-11 20:30:31 +0200

    Add files via upload

---
+++ virtual_machine/virtual_machine_main.tf
@@ -0,0 +1,125 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+    }
+  }
+}
+#########################################
+# Configure the Microsoft Azure Provider
+#########################################
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  #client_id = var.client_id
+  #client_secret = var.client_secret
+  tenant_id = var.tenant_id
+}
+
+########################################
+# refer to a resource group
+########################################
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.rg_name}-rg"
+  location = var.location
+}
+
+#######################################
+# Reference Azure Key Vault
+#######################################
+
+data "azurerm_key_vault" "secretkv" {
+  name                = var.keyvaultname
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+######################################
+# Reference Key Vault Secret
+######################################
+
+data "azurerm_key_vault_secret" "secret1" {
+  name         = var.user
+  key_vault_id = data.azurerm_key_vault.existing.id
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = data.azurerm_key_vault_secret.secret1.value
+  location = var.location
+}
+
+#########################################
+#refer to a subnet
+#########################################
+
+data "azurerm_subnet" "vnet" {
+  name                 = var.subnetname
+  virtual_network_name = var.vnetname
+  resource_group_name  = var.existingrg
+}
+
+#########################################
+# create a network interface
+#########################################
+
+resource "azurerm_network_interface" "nic" {
+  count               = var.count
+  name                = "${var.vm_name}-${count.index + 1}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_servers         = var.dns_servers
+
+  ip_configuration {
+    name                          = "${var.prefix}-${count.index + 1}-nic"
+    subnet_id                     = data.azurerm_subnet.subnet.id
+    private_ip_address_allocation = "dynamic"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg-nic-join" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+########################################################
+#Create Virtual Machine
+########################################################
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  depends_on = [
+    azurerm_network_interface.nic,
+    var.rg_name,
+  ]
+
+  count                 = var.vm_count
+  name                  = "${var.vm_name}-${count.index + 1}"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = var.vm_size
+  provision_vm_agent    = true
+  admin_username        = data.azurerm_key_vault.secret.secretkv.name
+  admin_password        = data.azurerm_key_vault.secret.secretkv.value
+  network_interface_ids = ["${azurerm_network_interface.nic.*.id[count.index]}"]
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  os_disk {
+    name                 = "${lower(var.vm_name)}-${count.index + 1}"
+    caching              = "ReadWrite"
+    storage_account_type = var.storageat
+  }
+}
+
+source_image_reference {
+  publisher = var.publisher
+  offer     = var.offer
+  sku       = var.sku
+  version   = "latest"
+}
---
+++ virtual_machine/virtual_machine_variables.tf
@@ -0,0 +1,96 @@
+# variable "subscription_id" {
+#     type = string
+#     description = "Variable for our resource group"
+# }
+# variable "tenant_id" {
+#     type = string
+#     description = "Variable for our resource group"
+# }
+# variable "client_id" {
+#     type = string
+#     description = "Variable for our resource group"
+# }
+# variable "client_secret" {
+#     type = string
+#     description = "Variable for our resource group"
+# }
+variable "subscription_id" {
+  type        = string
+  description = "Existing VM subscription ID"
+}
+
+variable "tenant_id" {
+  type        = string
+  default     = "1878a48b-63d6-4d12-a900-07d4267f6762"
+  description = "Tenant ID phoenixonline"
+}
+
+variable "rg-name" {
+  type        = string
+  description = "name of your resource group"
+
+}
+variable "location" {
+  default     = "westeurope"
+  description = "location of your resource group"
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix for the virtual machine"
+}
+
+variable "vm_name" {
+  type        = string
+  description = "virtual machine name"
+}
+
+variable "vnet-rg" {
+  type        = string
+  description = "Existing virutal network resource group"
+}
+
+variable "vnet" {
+  type        = string
+  description = "Existing virutal network"
+}
+variable "subnet" {
+  type        = string
+  description = "Existing virutal network subnet name"
+}
+
+variable "publisher" {
+  type        = string
+  description = "Name of OS publisher"
+}
+
+variable "offer" {
+  type        = string
+  description = "Name of OS offer e.g. office-365"
+}
+
+variable "sku" {
+  type        = string
+  description = "OS sku"
+}
+
+variable "vm_count" {
+  type        = number
+  description = "Number of virtual machines to deploy"
+}
+
+variable "vm_size" {
+  type        = string
+  description = "Size of the machine to deploy"
+}
+
+variable "dns_servers" {
+  description = "On-premise DNS Server"
+  default     = ["10.156.70.11", "10.156.70.13"]
+}
+
+variable "user" {
+  type        = string
+  default     = "adminusr"
+  description = "Local administrator account"
+}
