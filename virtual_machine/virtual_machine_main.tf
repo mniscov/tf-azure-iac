3548723d1a35f1ddecbfd4b8a57e9b2a8220bcb9
mniscov <marian.niscov@kronsoft.ro>
2024-12-13 00:28:50 +0200

    Update virtual_machine_main.tf

--- virtual_machine/virtual_machine_main.tf
+++ virtual_machine/virtual_machine_main.tf
@@ -13,13 +13,13 @@ terraform {
 # Configure the Microsoft Azure Provider
 #########################################
 
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
+#provider "azurerm" {
+#  features {}
+#  subscription_id = var.subscription_id
   #client_id = var.client_id
   #client_secret = var.client_secret
-  tenant_id = var.tenant_id
-}
+ # tenant_id = var.tenant_id
+#}
 
 ########################################
 # refer to a resource group
