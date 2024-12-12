e06f502c050248a69c4c29b8d0328499440b1803
mniscov <marian.niscov@kronsoft.ro>
2024-12-12 22:13:11 +0200

    Update virtual_machine_main.tf

--- virtual_machine/virtual_machine_main.tf
+++ virtual_machine/virtual_machine_main.tf
@@ -80,11 +80,6 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_network_interface_security_group_association" "nsg-nic-join" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
 ########################################################
 #Create Virtual Machine
 ########################################################
