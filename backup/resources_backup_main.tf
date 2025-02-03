d9f1e60d952bcc07cb250c5c92e94175aac9f221
mniscov <marian.niscov@kronsoft.ro>
2025-02-03 18:44:18 +0200

    Update resources_backup_main.tf

--- backup/resources_backup_main.tf
+++ backup/resources_backup_main.tf
@@ -1,5 +1,6 @@
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
+  location = var.resource_group_location
 }
 
 resource "azurerm_recovery_services_vault" "bk-vault" {
@@ -20,8 +21,20 @@ resource "azurerm_backup_policy_vm" "daily-policy" {
     frequency = var.backup_frequency
     time      = var.backup_time
   }
-
   retention_daily {
     count = var.retention_daily_count
   }
 }
+resource "azurerm_backup_protected_vm" "vm_backup" {
+  for_each = { 
+    for vm in azurerm_virtual_machine.this : vm.name => vm 
+    if lookup(vm.tags, "backup", "no") == "yes" && lookup(vm.tags, "managedBy", "none") == "terraform"
+  }
+
+  resource_group_name = data.azurerm_resource_group.rg.name
+  recovery_vault_name = azurerm_recovery_services_vault.bk-vault.name
+  source_vm_id        = each.value.id
+  backup_policy_id    = azurerm_backup_policy_vm.daily-policy.id
+}
+
+
