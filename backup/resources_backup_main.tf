4d454976362adfc7b93126bcec7d3c520d78c10a
mniscov <marian.niscov@kronsoft.ro>
2025-02-03 22:05:44 +0200

    Update resources_backup_main.tf

--- backup/resources_backup_main.tf
+++ backup/resources_backup_main.tf
@@ -9,7 +9,6 @@ resource "azurerm_recovery_services_vault" "bk-vault" {
   sku                 = var.vault_sku
   public_network_access_enabled = var.public_network_access_enabled
   storage_mode_type   = var.storage_mode_type
-  soft_delete_enabled = false
   tags = var.tags
 }
 
