f2f3ce939c0bfe0e60f2e4c278805623dff61882
mniscov <marian.niscov@kronsoft.ro>
2025-02-05 20:11:23 +0200

    Update virtual_machine_outputs.tf

--- compute/azurerm_linux_virtual_machine/virtual_machine_outputs.tf
+++ compute/azurerm_linux_virtual_machine/virtual_machine_outputs.tf
@@ -7,3 +7,7 @@ output "vm_name" {
   description = "Base name of the virtual machines"
   value       = var.vm_name
 }
+output "vm_ids" {
+  description = "List of VM IDs created"
+  value       = azurerm_linux_virtual_machine.vm[*].id
+}
