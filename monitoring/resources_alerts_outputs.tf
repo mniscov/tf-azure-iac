640eaae9537b20b41451011a423aad51f415f5a5
mniscov <marian.niscov@kronsoft.ro>
2025-02-05 20:48:21 +0200

    Update resources_alerts_outputs.tf

--- monitoring/resources_alerts_outputs.tf
+++ monitoring/resources_alerts_outputs.tf
@@ -1,11 +1,1 @@
-output "action_group_id" {
-  value = azurerm_monitor_action_group.action_group.id
-}
 
-output "log_alert_name" {
-  value = azurerm_monitor_activity_log_alert.log_alert.name
-}
-
-output "resource_group_name" {
-  value = data.azurerm_resource_group.rg.name
-}
