b97d9ae7d12a77ed1b5d1f12047767476091130f
mniscov <marian.niscov@kronsoft.ro>
2025-03-26 14:29:42 +0200

    Update variables.tf

--- vmss/azurerm_windows_vmss/variables.tf
+++ vmss/azurerm_windows_vmss/variables.tf
@@ -1,62 +1,99 @@
-variable "vmss_name" {
-  description = "The name of the VMSS"
+variable "resource_group_name" {
+  description = "The name of the existing resource group"
   type        = string
 }
 
 variable "location" {
-  description = "The location for all resources"
+  description = "The location of the resource group"
   type        = string
 }
 
-variable "vm_size" {
-  description = "The size of the VM instances"
+variable "vnet_name" {
+  description = "The name of the existing virtual network"
   type        = string
-  default     = "Standard_DS1_v2"
 }
 
-variable "instance_count" {
-  description = "Number of instances in the VMSS"
+variable "address_space" {
+  description = "The address space of the virtual network"
+  type        = list(string)
+}
+
+variable "subnet_name" {
+  description = "The name of the existing subnet"
+  type        = string
+}
+
+variable "subnet_address_prefix" {
+  description = "The address prefix of the subnet"
+  type        = list(string)
+}
+
+variable "vmss_name" {
+  description = "The name of the virtual machine scale set"
+  type        = string
+}
+
+variable "vmss_sku" {
+  description = "The SKU of the virtual machine scale set"
+  type        = string
+}
+
+variable "vmss_instances" {
+  description = "The number of instances in the virtual machine scale set"
   type        = number
-  default     = 0
-}
-
-variable "admin_username" {
-  description = "The administrator username for the VM instances"
-  type        = string
 }
 
 variable "admin_password" {
-  description = "The administrator password for the VM instances"
+  description = "The admin password for the virtual machine"
   type        = string
-  sensitive   = true
+}
+
+variable "admin_username" {
+  description = "The admin username for the virtual machine"
+  type        = string
+}
+
+variable "computer_name_prefix" {
+  description = "The prefix for the computer names in the scale set"
+  type        = string
 }
 
 variable "image_publisher" {
-  description = "The publisher of the image"
+  description = "The publisher of the virtual machine image"
   type        = string
-  default     = "MicrosoftWindowsServer"
 }
 
 variable "image_offer" {
-  description = "The offer of the image"
+  description = "The offer of the virtual machine image"
   type        = string
-  default     = "WindowsServer"
 }
 
 variable "image_sku" {
-  description = "The SKU of the image"
+  description = "The SKU of the virtual machine image"
   type        = string
-  default     = "2019-Datacenter"
 }
 
 variable "image_version" {
-  description = "The version of the image"
+  description = "The version of the virtual machine image"
   type        = string
-  default     = "latest"
 }
 
-variable "disk_size_gb" {
-  description = "The size of the disk in GB"
-  type        = number
-  default     = 200
+variable "os_disk_storage_account_type" {
+  description = "The storage account type for the OS disk"
+  type        = string
+}
+
+variable "os_disk_caching" {
+  description = "The caching setting for the OS disk"
+  type        = string
+}
+
+variable "network_interface_name" {
+  description = "The name of the network interface"
+  type        = string
+}
+
+variable "ip_configuration_name" {
+  description = "The name of the IP configuration for the network interface"
+  type        = string
 }
